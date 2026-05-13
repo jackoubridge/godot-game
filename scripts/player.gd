e577819f9badcc2d5b152f17b17132ea74e80884
jackoubridge <jaoubridge@gmail.com>
2026-05-14 00:29:25 +0100

    Diep clone

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,16 @@
-extends CharacterBody3D
+extends CharacterBody2D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@export var speed: float = 200
+@export var acceleration: float = 10
+@export var friction: float = 5
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var velocity_x: float = 1.0 - exp( -(acceleration if input.x else friction) * delta)
+	velocity.x = lerp(velocity.x, input.x * speed, velocity_x)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var velocity_y: float = 1.0 - exp( -(acceleration if input.y else friction) * delta)
+	velocity.y = lerp(velocity.y, input.y * speed, velocity_y)
 
 	move_and_slide()

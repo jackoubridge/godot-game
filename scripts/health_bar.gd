0f0fe69ffbe952607fc19ddffdd71d6200d95fd6
jackoubridge <jaoubridge@gmail.com>
2026-05-17 20:53:28 +0100

    Fix progress bar buggy on Windows

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -2,16 +2,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$ProgressBar.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if ($ProgressBar.value == 1):
-		$ProgressBar.visible = false
-	else:
+	if ($ProgressBar.value != 1 and $ProgressBar.visible == false):
 		$ProgressBar.visible = true
-		
-	$ProgressBar.value = lerp($ProgressBar.value, get_parent().health / get_parent().health_max, 25*delta)
 
 func _physics_process(_delta: float) -> void:
-	pass
+	$ProgressBar.value = get_parent().health / get_parent().health_max

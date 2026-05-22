d886e5cef1ed75ae60a5ed1722425a378352e960
jackoubridge <jaoubridge@gmail.com>
2026-05-23 00:20:46 +0100

    Target death animation

--- scripts/target.gd
+++ scripts/target.gd
@@ -23,11 +23,24 @@ func take_damage(source: Area2D):
 	$"Health Bar".update()
 	last_damage_source = source.owner_node
 	if health <= 0:
-		global_signals.target_destroyed.emit(self)
-		if last_damage_source != null:
-			last_damage_source.add_xp(xp_value)
-		queue_free()
+		destroy()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
 		take_damage(area)
+
+func destroy():
+
+	global_signals.target_destroyed.emit(self)
+	if last_damage_source != null:
+		last_damage_source.add_xp(xp_value)
+
+	# Death animation
+	$Area2D/CollisionShape2D.set_deferred("disabled", true)
+	$"Health Bar".queue_free()
+	var tween = create_tween()
+	tween.parallel().tween_property(self, "scale", Vector2.ZERO, 0.12)
+	tween.parallel().tween_property(self, "modulate:a", 0.0, 0.12)
+	await tween.finished
+
+	queue_free()

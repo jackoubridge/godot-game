ae42796fff043d0d9e798229a9bac6573b7869e5
jackoubridge <jaoubridge@gmail.com>
2026-05-28 23:07:22 +0100

    Moved gun logic to separate gun script

---
+++ scripts/gun.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+@export var recoil_offset: Vector2 = Vector2(-5, 0)
+
+var rest_position: Vector2
+var recoil_tween: Tween
+
+var can_shoot: bool = true
+
+@export var player: Node2D
+@export var bullet_path: PackedScene
+
+func _ready() -> void:
+	rest_position = position
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("shoot"):
+		shoot()
+
+func shoot() -> void:
+	if can_shoot:
+		# Handle shoot cooldown
+		can_shoot = false
+		$Timer.start(player.shoot_cooldown)
+
+		# Spawn bullet
+		var bullet = bullet_path.instantiate()
+
+		bullet.owner_node = player
+		bullet.pos = $BulletStart.global_position
+		bullet.dir = get_parent().rotation
+		bullet.rot = get_parent().global_rotation
+		bullet.speed = player.bullet_speed
+		bullet.damage = player.bullet_damage
+
+		player.add_child(bullet)
+
+		# Play gun recoil animation
+		recoil()
+
+func recoil() -> void:
+	if recoil_tween:
+		recoil_tween.kill()
+
+	recoil_tween = create_tween()
+
+	recoil_tween.tween_property(self, "position", rest_position + recoil_offset, 0.05)
+	recoil_tween.tween_property(self, "position", rest_position, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	get_parent().knockback(player.recoil_strength)
+
+# Handle shoot cooldown
+func _on_timer_timeout() -> void:
+	can_shoot = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,42 +7,15 @@ extends Node2D
 @export var recoil_strength: float = bullet_speed / 3
 @export var health_max: float = 10
 
-@export var bullet_path: PackedScene
-
 signal xp_update(value: int)
 signal level_update(value: int)
 
 var level:int = 1
 var level_up_xp: int = 10
 var xp: int = 0
-var can_shoot: bool = true
+
 var health: float = health_max
 var shoot_cooldown: float = 0.5
-
-func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("shoot"):
-		shoot()
-
-func shoot() -> void:
-	if can_shoot:
-		# Handle shoot cooldown
-		can_shoot = false
-		$CharacterBody2D/Gun/Timer.start(shoot_cooldown)
-
-		# Spawn bullet
-		var bullet = bullet_path.instantiate()
-
-		bullet.owner_node = self
-		bullet.pos = $CharacterBody2D/Gun/BulletStart.global_position
-		bullet.dir = $CharacterBody2D.rotation
-		bullet.rot = $CharacterBody2D.global_rotation
-		bullet.speed = bullet_speed
-		bullet.damage = bullet_damage
-
-		add_child(bullet)
-
-		# Play gun recoil animation
-		$CharacterBody2D/Gun.recoil()
 
 func add_xp(amount) -> void:
 	xp += amount
@@ -54,7 +27,3 @@ func add_xp(amount) -> void:
 		level_up_xp = 5 * (2 ** level)
 		shoot_cooldown = max(0.5 ** level, 0.5 ** 5)
 	xp_update.emit(xp, level_up_xp)
-
-# Handle shoot cooldown
-func _on_timer_timeout() -> void:
-	can_shoot = true
